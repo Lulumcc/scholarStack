163a5ad1a6006efa489d089601b43f8f11199f7a
helloprof <suluxan.seneca@gmail.com>
2024-02-09 15:15:47 -0500

    serving html files in response

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express()
 
 const HTTP_PORT = 8080
 const scholarService = require("./modules/scholarService")
+const path = require("path")
 
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.redirect("/articles")
@@ -11,9 +13,17 @@ app.get("/", (req, res) => {
 
 app.get("/articles", (req, res) => {
     scholarService.getArticles().then((articles) => {
-        res.json(articles)
+        // res.json(articles)
+        res.sendFile(path.join(__dirname, "/views/index.html"))
+        console.log(articles)
     })
 })
+
+app.get("/about", (req, res) => {
+    scholarService.getArticles().then((articles) => {
+        // res.json(articles)
+        res.send(articles)
+    })})
 
 scholarService.initialize().then(() => {
     app.listen(HTTP_PORT, () => {
